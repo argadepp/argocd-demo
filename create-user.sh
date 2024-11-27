5836d1eba0d9e6bcc20d64a14540fc4837d39a46
argadepp <argadepp@gmail.com>
2024-11-27 14:42:27 +0530

    added storageclass

---
+++ create-user.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+user=$1
+## create role with name
+aws iam create-role --role-name $user --assume-role-policy-document file://manifest/trust-policy.yaml 
+
+aws iam attach-role-policy --role-name $user --policy-arn arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy
+
+aws iam attach-role-policy --role-name $user --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy
+
+aws iam attach-role-policy --role-name $user --policy-arn arn:aws:iam::637423592422:policy/eks-role-policy
+
+kubectl annotate serviceaccount $user -n utilities   eks.amazonaws.com/role-arn=arn:aws:iam::637423592422:role/pratik-argade
+
+aws eks update-kubeconfig \
+  --region ap-south-1 \
+  --name eks-monitoring-cluster \
+  --role-arn arn:aws:iam::637423592422:role/$user \
+  --kubeconfig ./kubeconfig-$user
+  
+eksctl create iamidentitymapping \
+--region ap-south-1 \
+--cluster eks-monitoring-cluster \
+--arn arn:aws:iam::637423592422:role/$user \
+--group system:masters \
+--username $user
